6cadbac3ebfbc8e6e001901f28c818cac815dc91
Allan Santosh Joseph <148749764+allanjoseph01@users.noreply.github.com>
2025-05-03 20:00:38 +0530

    Time: 0 ms (100%), Space: 21.9 MB (38.48%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int siz=nums.size();
+        for(int i=0;i<siz;i++){
+            while(nums[i]!=i+1){
+                int ele=nums[i];
+                int in=ele-1;
+                if(in<0 || in>=siz){
+                    break;
+                }
+                if(nums[i]==nums[in]){
+                    break;
+                }
+                int t=nums[in];
+                nums[in]=nums[i];
+                nums[i]=t;
+            }
+        }
+        for(int i=0;i<siz;i++){
+            if(nums[i]==0){
+                return i+1;
+            }
+        }
+        return 0;
+    }
+};
