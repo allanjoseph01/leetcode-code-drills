e20fcc251407d8e26717181e345c30b9b256e042
Allan Santosh Joseph <148749764+allanjoseph01@users.noreply.github.com>
2025-09-06 13:18:56 +0530

    Time: 4 ms (55.6%), Space: 11.5 MB (72.19%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    void unique(vector<int>& nums , int index){
-        int s=nums.size();
-        if(index==s){
-            ans.push_back(nums);
+    void alluniqueperm(int i,int n, vector<int>& arr,unordered_map<int,int>& mp , vector<vector<int>>& ans ){
+        if(i==n){
+            ans.push_back(arr);
             return ;
         }
-        unordered_set<int> st;
-        for(int i=index;i<s;i++){
-            if(st.find(nums[i])==st.end()){
-                st.insert(nums[i]);
-                swap(nums[i], nums[index]);
-                unique(nums,index+1);
-                swap(nums[i], nums[index]);
+        for(auto it : mp){
+            if(it.second>0){
+                arr.push_back(it.first);
+                mp[it.first]--;
+                alluniqueperm(i+1,n,arr,mp,ans);
+                mp[it.first]++;
+                arr.pop_back();
             }
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        unique(nums,0);
+        unordered_map<int,int> mp;
+        int siz= nums.size();
+        for(int i=0;i<siz;i++){
+            mp[nums[i]]++;
+        }
+        vector<vector<int>> ans;
+        vector<int> temp;
+        alluniqueperm(0,siz,temp , mp , ans);
         return ans;
     }
 };
