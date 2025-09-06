46b715b4c32f8f469b2232e2fc17de6641673528
Allan Santosh Joseph <148749764+allanjoseph01@users.noreply.github.com>
2025-09-06 13:08:33 +0530

    Time: 1 ms (42.62%), Space: 10.5 MB (86.77%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,62 +1,20 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
-        int siz=nums.size();
-        if(siz==1){
-            return;
-        }
-        int i;
-        for(i=siz-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                break;
-            }
-        }
-        if(i<0){
-            int k=0;
-            int t=siz-1;
-            while(k<t){
-                int temp=nums[k];
-                nums[k]=nums[t];
-                nums[t]=temp;
-                k++;
-                t--;
-            }
+    void allpermute(int i,int n, vector<int>& nums,vector<vector<int>>& ans){
+        if(i==n){
+            ans.push_back(nums);
             return ;
         }
-        int j;
-        for(j=siz-1;j>i;j--){
-            if(nums[j]>nums[i]){
-                break;
-            }
-        }
-        int temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
-        int k=i+1;
-        int t=siz-1;
-        while(k<t){
-            int temp=nums[k];
-            nums[k]=nums[t];
-            nums[t]=temp;
-            k++;
-            t--;
+        for(int j=i;j<n;j++){
+            swap(nums[i],nums[j]);
+            allpermute(i+1,n,nums,ans);
+            swap(nums[i],nums[j]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        vector<vector<int>> ans;
         int siz=nums.size();
-        int p=1;
-        int n=1;
-        while(n<=siz){
-            p*=n;
-            n++;
-        }
-        while(p--){
-            ans.push_back(nums);
-            nextPermutation(nums);
-        }
-        // ans.push_back(nums);
+        vector<vector<int>> ans;
+        allpermute(0,siz,nums,ans);
         return ans;
     }
 };
